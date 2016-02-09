a732375f86b9513e5c8e4540145f15021ed9622a
LeonTG77 <leontg77@gmail.com>
2016-02-09 12:30:12 +0100

    Resetting tab colors when the scenario gets disabled

--- src/com/leontg77/biomeparanoia/commands/BParanoiaCommand.java
+++ src/com/leontg77/biomeparanoia/commands/BParanoiaCommand.java
@@ -9,6 +9,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 
 import com.leontg77.biomeparanoia.Main;
@@ -80,6 +81,10 @@ public class BParanoiaCommand implements CommandExecutor, TabCompleter {
 			plugin.enabled = false;
 			
 			HandlerList.unregisterAll(listener);
+			
+			for (Player online : Bukkit.getOnlinePlayers()) {
+				online.setPlayerListName(null);
+			}
 			return true;
 		}
 		
