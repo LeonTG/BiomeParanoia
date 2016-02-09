55931cd1b0e5f1623cd8b4da181485d9cfa81273
LeonTG77 <leontg77@gmail.com>
2016-02-09 11:46:59 +0100

    Code improvement and a new prefix

--- src/com/leontg77/biomeparanoia/Main.java
+++ src/com/leontg77/biomeparanoia/Main.java
@@ -3,8 +3,9 @@ package com.leontg77.biomeparanoia;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.leontg77.biomeparanoia.cmds.BParanoiaCommand;
-import com.leontg77.biomeparanoia.cmds.BlCommand;
+import com.leontg77.biomeparanoia.commands.BParanoiaCommand;
+import com.leontg77.biomeparanoia.commands.BiomeListCommand;
+import com.leontg77.biomeparanoia.listeners.MoveListener;
 
 /**
  * Main class of the plugin.
@@ -12,33 +13,32 @@ import com.leontg77.biomeparanoia.cmds.BlCommand;
  * @author LeonTG77
  */
 public class Main extends JavaPlugin {
-	public static Main plugin;
+	public static final String PREFIX = "§5§lBiome Paranoia §8» §7";
 	
-	public static final String PREFIX = "§7[§6BP§7] §f";
-	public static boolean enabled = false;
+	public boolean enabled = false;
 
 	@Override
 	public void onDisable() {
-		// print a message to the console saying it has been disabled.
-		PluginDescriptionFile file = getDescription();
+		final PluginDescriptionFile file = getDescription();
 		getLogger().info(file.getName() + " has been disabled.");
-		
-		// set the plugin field to null.
-		plugin = null;
 	}
 	
 	@Override
 	public void onEnable() {
-		// print a message to the console saying it has been enabled.
-		PluginDescriptionFile file = getDescription();
+		final PluginDescriptionFile file = getDescription();
 		getLogger().info(file.getName() + " v" + file.getVersion() + " has been enabled.");
-		getLogger().info("Plugin is made by LeonTG77.");
+		getLogger().info("The plugin is made by LeonTG77.");
 		
-		// register the /bl and /bparanoia commands.
-		getCommand("bl").setExecutor(new BlCommand());
-		getCommand("bparanoia").setExecutor(new BParanoiaCommand());
+		final MoveListener listener = new MoveListener();
 		
-		// set the plugin field to this class.
-		plugin = this;
+		final BParanoiaCommand mainCommand = new BParanoiaCommand(this, listener);
+		final BiomeListCommand listCommand = new BiomeListCommand(this);
+		
+		// register command.
+		getCommand("bparanoia").setExecutor(mainCommand);
+		getCommand("bparanoia").setTabCompleter(mainCommand);
+		
+		getCommand("biomelist").setExecutor(listCommand);
+		getCommand("biomelist").setTabCompleter(listCommand);
 	}
 }
