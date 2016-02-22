45cf4381aba3855c6b59340bffed69675acdc5bb
LeonTG77 <leontg77@gmail.com>
2016-02-22 14:50:01 +0100

    Updated to using the update method

--- src/com/leontg77/biomeparanoia/listeners/MoveListener.java
+++ src/com/leontg77/biomeparanoia/listeners/MoveListener.java
@@ -1,6 +1,5 @@
 package com.leontg77.biomeparanoia.listeners;
 
-import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.block.Biome;
 import org.bukkit.entity.Player;
@@ -8,6 +7,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
+import com.leontg77.biomeparanoia.Main;
 import com.leontg77.biomeparanoia.Utils;
 
 /**
@@ -18,6 +18,11 @@ import com.leontg77.biomeparanoia.Utils;
  * @author LeonTG77
  */
 public class MoveListener implements Listener {
+	private final Main plugin;
+	
+	public MoveListener(Main plugin) {
+		this.plugin = plugin;
+	}
 
 	@EventHandler(ignoreCancelled = true)
 	public void on(PlayerMoveEvent event) {
@@ -37,23 +42,6 @@ public class MoveListener implements Listener {
 			return;
 		}
 		
-		String biomeColor = Utils.getBiomeColor(biome);
-		String name = player.getName();
-		 
-		try {
-			// if their gamemode is spectator mode, we don't want to set a color.
-			if (player.getGameMode() == GameMode.SPECTATOR) {
-				// setting to null to reset it incase they had a color.
-				player.setPlayerListName(null);
-				return;
-			}
-		} catch (Exception e) {
-			// The SPECTATOR enum wasn't found that means they are not using 1.8+
-			// 1.7 or lower didnt support 16+ caracter long names.
-			player.setPlayerListName(biomeColor + name.substring(0, Math.min(name.length(), 16 - biomeColor.length())));
-			return;
-		}
-		
-		player.setPlayerListName(biomeColor + name);
+		Utils.updatePlayer(player, plugin);
 	}
 }
